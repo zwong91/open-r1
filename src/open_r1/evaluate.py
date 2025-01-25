d0265f94ce9120ee344663f302594fc97bcbcff4
Quentin Gallouédec <45557362+qgallouedec@users.noreply.github.com>
2025-01-25 14:41:46 +0100

    Copyrights (#20)
    * handle error in verification
    
    * command with zero2 and catch more error in verifier
    
    * Update README.md
    
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>
    
    * deepseek distill and remove grad chekpoint
    
    * drop grad checkpoint
    
    * except
    
    * copyrights
    
    ---------
    
    Co-authored-by: lewtun <lewis.c.tunstall@gmail.com>

--- src/open_r1/evaluate.py
+++ src/open_r1/evaluate.py
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """Custom evaluation tasks for LightEval."""
 
 from lighteval.metrics.dynamic_metrics import (
--- src/open_r1/sft.py
+++ src/open_r1/sft.py
@@ -1,6 +1,4 @@
-#!/usr/bin/env python
-# coding=utf-8
-# Copyright 2025 The HuggingFace Inc. team. All rights reserved.
+# Copyright 2025 The HuggingFace Team. All rights reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -13,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 Supervised fine-tuning script for decoder language models.
 
