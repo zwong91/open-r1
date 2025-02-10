88c51fe05d682543f9685e3dc821494bf253d231
Edward Beeching <edbeeching@users.noreply.github.com>
2025-02-10 12:35:21 +0100

    Repetition penalty hotfix (#266)
    * Adds a Repetition Penalty Reward
    
    * style
    
    * adds option to configue in grpo
    
    * style
    
    * improve desciptions
    
    * fix final changes
    
    * fix docstring
    
    * style

--- src/open_r1/grpo.py
+++ src/open_r1/grpo.py
@@ -25,7 +25,13 @@ from transformers import set_seed
 from transformers.trainer_utils import get_last_checkpoint
 
 from open_r1.configs import GRPOConfig
-from open_r1.rewards import accuracy_reward, format_reward, get_cosine_scaled_reward, reasoning_steps_reward
+from open_r1.rewards import (
+    accuracy_reward,
+    format_reward,
+    get_cosine_scaled_reward,
+    get_repetition_penalty_reward,
+    reasoning_steps_reward,
+)
 from open_r1.utils.callbacks import get_callbacks
 from trl import GRPOTrainer, ModelConfig, ScriptArguments, TrlParser, get_peft_config
 
@@ -78,6 +84,15 @@ class GRPOScriptArguments(ScriptArguments):
     cosine_max_len: int = field(
         default=1000,
         metadata={"help": "Maximum length for scaling"},
+    )
+
+    repetition_n_grams: int = field(
+        default=3,
+        metadata={"help": "Number of n-grams for repetition penalty reward"},
+    )
+    repetition_max_penalty: float = field(
+        default=-1.0,
+        metadata={"help": "Maximum (negative) penalty for for repetition penalty reward"},
     )
 
 
@@ -138,6 +153,10 @@ def main(script_args, training_args, model_args):
             min_value_correct=script_args.cosine_min_value_correct,
             max_value_correct=script_args.cosine_max_value_correct,
             max_len=script_args.cosine_max_len,
+        ),
+        "repetition_penalty": get_repetition_penalty_reward(
+            ngram_size=script_args.repetition_n_grams,
+            max_penalty=script_args.repetition_max_penalty,
         ),
     }
     reward_funcs = [REWARD_FUNCS_REGISTRY[func] for func in script_args.reward_funcs]
