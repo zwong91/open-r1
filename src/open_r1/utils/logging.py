80e7e7b23c3e6694d94f1d719d69f2134504af66
Edward Beeching <edbeeching@users.noreply.github.com>
2025-02-13 11:13:00 +0100

    move details script and fix wandb logging (#314)

--- src/open_r1/utils/logging.py
+++
@@ -1,9 +0,0 @@
-import os
-
-
-def init_wandb_training(training_args):
-    """
-    Helper function for setting up Weights & Biases logging tools.
-    """
-    os.environ["WANDB_ENTITY"] = training_args.wandb_entity
-    os.environ["WANDB_PROJECT"] = training_args.wandb_project
